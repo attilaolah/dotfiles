58dc43812f8f4478f9a5fb4dab5e5f21ca7909f5
Attila Oláh <atl@google.com>
2022-12-12 22:06:26 +0100

    Drop whitespace prefix when no SSH or virtualenv present.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -40,7 +40,7 @@ function show_user -d "Show user"
   if not contains $USER $default_user; or test -n "$SSH_CLIENT"
     set -l host (hostname -s)
     set -l who (whoami)
-    prompt_segment normal yellow " $who"
+    prompt_segment normal yellow "$who"
 
     # Skip @ bit if hostname == username
     if [ "$USER" != "$HOST" ]
