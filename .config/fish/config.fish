402700defce3e4c3b61ad1967ec894f18495c93f
Attila Oláh <atl@google.com>
2022-12-12 22:28:07 +0100

    Fix (and simplify) git-log abbrev.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,9 +24,9 @@ if status is-interactive
     abbr --add ga git add -p .
     abbr --add gb git branch -avv
     abbr --add gc git commit -v
+    abbr --add gl git log --abbrev-commit --decorate --graph --pretty=oneline
     abbr --add gp git push
     abbr --add gr git remote -v
-    abbr --add gl git log --graph --abbrev-commit --pretty=\"format:(set_color -o -u brred)%h(set_color normal)(set_color yellow)%d(set_color normal) (set_color red)\(%cr\)(set_color normal) %s — (set_color blue)%ae(set_color normal)\"
 
     function gg
         git commit -m "$argv"
