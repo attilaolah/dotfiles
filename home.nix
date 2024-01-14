0c140c405b7d7f4f0607cd0b0b30950a11f8c18a
Attila Oláh <attilaolah@gmail.com>
2024-01-14 22:19:31 +0100

    Add Hyprland wallpaper.

--- home.nix
+++ home.nix
@@ -65,8 +65,10 @@
     ".config/fish/functions/fish_prompt.fish".source = ./src/_.config/fish/functions/fish_prompt.fish;
     ".config/foot/foot.ini".source = ./src/_.config/foot/foot.ini;
     ".config/hypr/hyprland.conf".source = ./src/_.config/hypr/hyprland.conf;
+    ".config/hypr/hyprpaper.conf".source = ./src/_.config/hypr/hyprpaper.conf;
     ".config/nvim/init.lua".source = ./src/_.config/nvim/init.lua;
     ".config/sway/config".source = ./src/_.config/sway/config;
+    ".config/wallpapers/alpeli-1020m.jpg".source = ./src/_.config/wallpapers/alpeli-1020m.jpg;
     # ~/.*:
     ".bash_profile".source = ./src/_.bash_profile;
     ".bashrc".source = ./src/_.bashrc;
